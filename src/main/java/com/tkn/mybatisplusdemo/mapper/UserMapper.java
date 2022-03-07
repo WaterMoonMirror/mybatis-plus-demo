439b0549faa8192281a7792dc97fd159869ae757
李柱 <lizhu@wondersgroup.com>
2022-03-07 11:17:12 +0800

    3、代码生成器 （1）AutoGenerator 简介 　　AutoGenerator 是 MyBatis-Plus 的代码生成器，通过 AutoGenerator 可以快速生成 Entity、Mapper、Mapper XML、Service、Controller 等各个模块的代码，极大的提升了开发效率。 　　与 mybatis 中的 mybatis-generator-core 类似。

--- src/main/java/com/tkn/mybatisplusdemo/mapper/UserMapper.java
+++
@@ -1,8 +0,0 @@
-package com.tkn.mybatisplusdemo.mapper;
-
-
-import com.baomidou.mybatisplus.core.mapper.BaseMapper;
-import com.tkn.mybatisplusdemo.entity.User;
-
-public interface UserMapper extends BaseMapper<User> {
-}
